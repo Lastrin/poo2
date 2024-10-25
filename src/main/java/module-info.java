760dc28bd85a981d4e69033d243b24ad00aa1fb2
BrunoRodriguesGO <153209849+BrunoRodriguesGO@users.noreply.github.com>
2024-10-25 20:00:01 -0300

    Update projetinho

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.projetinho {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    //requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.projetinho to javafx.fxml;
+    exports org.projetinho;
+}
